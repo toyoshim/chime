6bd4a0cca8cbcd1b0e7f8772a93534e564ccc4ba
Takashi Toyoshima <toyoshim@gmail.com>
2012-12-31 07:37:20 -0800

    Initial commit for chime-0.1

---
+++ Makefile
@@ -0,0 +1,38 @@
+all:
+	@echo "build    ... compile sources and generate chime-*.min.js"
+	@echo "checkout ... checkout proper versions of libraries"
+	@echo "clean    ... delete libraries"
+	@echo "clone    ... clone libraries from public repositories"
+	@echo
+	@echo " * build may require node.js v0.8.16+, and UglifyJS"
+
+cat:
+	cat \
+		compat.js \
+		tss/js/tss/AudioLooper.js \
+		tss/js/tss/MasterChannel.js \
+		tss/js/tss/TssChannel.js \
+		tss/js/tss/TString.js \
+		tss/js/tss/TsdPlayer.js \
+		tss/js/tss/TssCompiler.js \
+		chime.js \
+		> chime-0.1.cat.js
+
+build:
+	closure_compiler \
+		--compilation_level ADVANCED_OPTIMIZATIONS \
+		--js compat.js \
+		--js tss/js/tss/AudioLooper.js \
+		--js tss/js/tss/MasterChannel.js \
+		--js tss/js/tss/TssChannel.js \
+		--js tss/js/tss/TString.js \
+		--js tss/js/tss/TsdPlayer.js \
+		--js tss/js/tss/TssCompiler.js \
+		--js chime.js \
+		--js_output_file chime-0.1.min.js
+
+checkout:
+	(cd tss; hg checkout b50a71f205dd)
+
+clone:
+	hg clone https://code.google.com/p/tss/
---
+++ chime.js
@@ -0,0 +1,151 @@
+/**
+ * @constructor
+ */
+window['chime'] = new (function chime() {
+  this.looper = new AudioLooper();
+  this.master = new MasterChannel();
+  this.master.setVolume(1.0);
+  this.looper.setChannel(this.master);
+  this.player = [];
+  this.effects = 1;
+  this.effectId = 0;
+  this.setupChannel = function (n) {
+    this.player[n] = new TsdPlayer();
+    this.player[n].device = new TssChannel();
+    this.player[n].device.setPlayer(this.player[n]);
+    this.master.addChannel(this.player[n].device);
+  }
+  for (var i = 0; i <= this.effects; ++i) this.setupChannel(i); })(); window['chime']['setMaxEffect'] = function(n) {
+  if (window['chime'].effects < n) {
+    for (var i = window['chime'].effects + 1; i <= n; ++i) {
+      window['chime'].setupChannel(i);
+    }
+  } else {
+    for (var i = n + 1; i <= window['chime'].effects; ++i) {
+      window['chime'].master.removeChannel(window['chime'].player[i].device);
+      window['chime'].player[i] = null;
+    }
+  }
+  window['chime'].effects = n;
+}
+
+window['chime']['maxEffect'] = function() {
+  return window['chime'].effects;
+}
+
+window['chime']['bgm'] = function(data) {
+  return window['chime']['play'](0, data);
+}
+
+window['chime']['effect'] = function(data) {
+  return window['chime']['play'](-1, data);
+}
+
+window['chime']['play'] = function(id, data) {
+  if (id > window['chime'].effects)
+    return false;
+  if (!data) {
+    // TODO: Implement stop
+  } else if (data.loading) {
+    data.willPlay = true;
+    data.playerId = id;
+  }
+  if (id < 0) {
+    window['chime'].effectId =
+        (window['chime'].effectId + 1) % window['chime'].effects;
+    id = window['chime'].effectId + 1;
+  }
+  window['chime'].player[id].play(data['tsd']);
+  return true;
+}
+
+/**
+ * @constructor
+ */
+window['chime']['Sound'] = function(data) {
+  this.loading = true;
+  this.willPlay = false;
+  this.playerId = -1;
+  this.log = '';
+  this.success = false;
+
+  if (typeof data === 'string' && data.indexOf('http') == 0) {
+    // URL fetch.
+    var xhr = new XMLHttpRequest();
+    xhr.owner = this;
+    xhr.open('get', data, true);
+    xhr.responseType = 'arraybuffer';
+    xhr.onload = function() {
+      this.owner.set(this.response);
+    }
+    xhr.send();
+    return;
+  }
+  if (data.constructor == Array) {
+    // MML generation.
+    var mml = [
+      '#TITLE <>',
+      '#CHANNEL ' + data.length
+    ];
+    var atoz = 'Z'.charCodeAt(0) - 'A'.charCodeAt(0);
+    var unit = atoz + 1;
+    var maxChannel = unit * unit + atoz;
+    if (data.length > maxChannel) {
+      this.log('too many channels: ' + data.length);
+      return;
+    }
+    for (var i = 0; i < data.length; ++i) {
+      var ch;
+      if (i < unit) {
+        ch = String.fromCharCode('A'.charCodeAt(0) + i);
+      } else {
+        var prefix = ~~(i / unit) - 1;
+        ch = String.fromCharCode('A'.charCodeAt(0) + prefix) +
+            String.fromCharCode('A'.charCodeAt(0) + (i % unit));
+      }
+      mml.push('#' + ch + ' ' + data[i]);
+    }
+    this.set(mml.join('\n'));
+  }
+
+  // Data.
+  this.set(data);
+}
+
+window['chime']['Sound'].prototype.set = function(data) {
+  this.loading = false;
+
+  if (typeof data === 'string') {
+    // Handle data as TSS.
+    var compiler = new TssCompiler();
+    Log.on();
+    this['tsd'] = compiler.compile(data);
+    Log.off();
+    this.log = Log.flush();
+    this.success = this['tsd'] != null;
+  } else if (data.constructor == ArrayBuffer) {
+    var view = new DataView(data);
+    if (view.getUint8(0) == 'T'.charCodeAt(0)) {
+      // Handle data as TSD.
+      this['tsd'] = data;
+      this.success = true;  // TODO: Should be checked.
+    } else {
+      // Handle data as TSS.
+      var compiler = new TssCompiler();
+      Log.on();
+      this['tsd'] = compiler.compile(data);
+      Log.off();
+      this.log = Log.flush();
+      this.success = this['tsd'] != null;
+    }
+  } else {
+    // TODO: Support other cases.
+    return;
+  }
+  if (this.willPlay)
+    window['chime']['play'](this.playerId, this);
+}
+
+window['chime']['createSound'] = function(data) {
+  return new window['chime']['Sound'](data);
+}
